3bb2e40f377382d463abd122546488d92530e0d1
Pablo Nieves <53600701+pnieves2019@users.noreply.github.com>
2020-08-20 13:09:26 +0200

    Update

--- maelas/maelas/data.py
+++
@@ -1,8 +0,0 @@
-""" Data structures """
-
-class SymmetryData:
-    def __init__(self):
-        self.number_of_species = -1
-        self.structure         = None
-        self.space_group       = -1
-        self.point_group       = -1
